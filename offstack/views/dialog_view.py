bcf3155b4b2942d01024d997ea584f1aad06efa9
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-05-20 22:11:42 +0200

    Dialog now follows MVP architecture

--- offstack/views/dialog_view.py
+++ offstack/views/dialog_view.py
@@ -1,20 +1,119 @@
+import os
 from threading import Thread
 
-class DialogHandlers():
-    def __init__(self, interface, queue):
+from offstack.constants import CURRDIR
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import  GObject as gobject
+
+class DialogView:
+    def __init__(self, interface, queue, Gtk):
+        interface.add_from_file(os.path.join(CURRDIR, "resources/dialog_window.glade"))
+        
+        interface.connect_signals({
+            "close_dialog_button_clicked": self.close_dialog_button_clicked,
+            "MessageDialog_delete_event": self.MessageDialog_delete_event,
+        })
+
+        self.messagedialog_window = interface.get_object("MessageDialog")
+        self.dialog_header = interface.get_object("dialog_header")
+        self.primary_text_title = interface.get_object("primary_text_title")
+        self.message_dialog_spinner = interface.get_object("message_dialog_spinner")
+
+        self.close_dialog_button = interface.get_object("close_dialog_button")
+
         self.interface = interface
         self.queue = queue
+        self.interface = interface
 
-        thread = Thread(target=listener, args=[self.interface, self.queue])
+        thread = Thread(target=self.listener)
         thread.daemon = True
         thread.start()
 
-    def close_dialog_button_clicked(self, button):
-        self.interface.get_object("MessageDialog").hide()
+    def listener(self):
+        while True:
+            kwargs = self.queue.get()
 
+            try:
+                if "display_dialog" in kwargs.get("action"):
+                    # self.display_dialog(**kwargs)
+                    gobject.idle_add(self.display_dialog, kwargs)
+                    self.queue.task_done()
+                if "update_dialog" in kwargs.get("action"):
+                    # self.update_dialog(**kwargs)
+                    gobject.idle_add(self.update_dialog, kwargs)
+                    self.queue.task_done()
+
+                if "hide_dialog" in kwargs.get("action"):
+                    # self.hide_dialog()
+                    gobject.idle_add(self.hide_dialog)
+                    self.queue.task_done()
+                    
+                if "hide_spinner" in kwargs.get("action"):
+                    # self.hide_spinner()
+                    gobject.idle_add(self.hide_spinner)
+                    self.queue.task_done()
+            except TypeError:
+                print("here")
+                pass
+                # gui_logger.debug(">>> Error occurs due to testing.") 
+            print("hello")
+    def display_dialog(self, kwargs):
+        self.close_dialog_button.show()
+        self.message_dialog_spinner.hide()
+        self.dialog_header.hide()
+
+        if "header" in kwargs:
+            self.dialog_header.set_markup(kwargs.get("header")) 
+            self.dialog_header.show()
+
+        if "label" in kwargs:
+            self.primary_text_title.set_markup(kwargs.get("label")) 
+
+        if "spinner" in kwargs and kwargs.get("spinner"):
+            self.message_dialog_spinner.show()
+
+        if "hide_close_button" in kwargs and kwargs.get("hide_close_button"):
+            self.close_dialog_button.hide()
+            self.messagedialog_window.connect("destroy", self.gtk.main_quit)
+        
+        self.messagedialog_window.show()
+
+    def update_dialog(self, kwargs):
+        self.close_dialog_button.show()
+        self.message_dialog_spinner.hide()
+        self.dialog_header.hide()
+
+        if "header" in kwargs:
+            self.dialog_header.set_markup(kwargs.get("header")) 
+            self.dialog_header.show()
+
+        if "label" in kwargs:
+            self.primary_text_title.set_markup(kwargs.get("label")) 
+
+        if "spinner" in kwargs and kwargs.get("spinner"):
+            self.message_dialog_spinner.show()
+
+        if "hide_close_button" in kwargs and kwargs.get("hide_close_button"):
+            self.close_dialog_button.hide()
+            self.messagedialog_window.connect("destroy", self.gtk.main_quit)            
+
+    def hide_spinner(self):
+        self.message_dialog_spinner.hide()
+
+    def hide_dialog(self):
+        self.messagedialog_window.hide()
+
+    def close_dialog_button_clicked(self, button):
+        """Button/Event handler to close message dialog.
+        """
+        self.interface.get_object("MessageDialog").hide()
+        
+    # To avoid getting the MessageDialog destroyed and not being re-rendered again
     def MessageDialog_delete_event(self, window, event):
         """On Delete handler is used to hide the dialog and so that it successfully renders next time it is called
-
+        
         -Returns:Boolean
         - It needs to return True, otherwise the content will not re-render after closing the window
         """
@@ -22,19 +121,4 @@ class DialogHandlers():
             window.hide()
             return True
 
-def listener(interface, queue):
-    dialog_header = interface.get_object("dialog_header")
-    primary_text_title = interface.get_object("primary_text_title")
-    message_dialog_spinner = interface.get_object("message_dialog_spinner")
-    while True:
-        item = queue.get()
-        if item["create_user"]:
-            if not item["create_user"]["response"]:
-                dialog_header.set_text("Error")
-                primary_text_title.set_text("Unable to extract token! (It could be related to incorrect credentials)")
-                message_dialog_spinner.hide()
-            else:
-                dialog_header.set_text("Setup is complete!")
-                primary_text_title.hide()
-                message_dialog_spinner.hide()
                 
