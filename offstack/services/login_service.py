69afa49105c3007d82703568c3889bb72e3a7900
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-05-21 13:29:29 +0200

    Updated params names

--- offstack/services/login_service.py
+++ offstack/services/login_service.py
@@ -1,13 +1,14 @@
 from offstack.constants import USERDATA
 
 class LoginService:
-    def create_user_file(self, email, password):
+    def create_user_file(self, username, password):
         try:
             with open(USERDATA, 'w') as f:
-                f.write("{0}\n{1}".format(email, password))
+                f.write("{0}\n{1}".format(username, password))
                 print()
                 print("User data saved to file!")
                 # logger.debug("User data saved to file!")
-                return True
         except:
             return False
+            
+        return True
